3a09c9a99f7d13e13b2ca7c752f368e8f9b5efff
Mylo Faulkner <mylofaulkner27@gmail.com>
2024-03-27 22:06:46 +0000

    feat: info display on correct arg

--- element.sh
+++ element.sh
@@ -13,6 +13,10 @@ ELEMENT=$($PSQL "SELECT name FROM elements WHERE atomic_number::text = '$1' OR s
 if [[ -z $ELEMENT ]]
 then
   echo "I could not find that element in the database."
-  exit
+else
+  echo "$($PSQL "SELECT * FROM types JOIN properties USING(type_id) JOIN elements USING(atomic_number) WHERE name = '$ELEMENT'")" | while IFS="|" read ATOMIC_NUMBER TYPE_ID TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS SYMBOL NAME 
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+  done 
 fi
 

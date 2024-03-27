657f9f2a5b762c92dddba197fc010779d77cc234
Mylo Faulkner <mylofaulkner27@gmail.com>
2024-03-27 21:20:39 +0000

    feat: output when element isn't found

--- element.sh
+++ element.sh
@@ -5,5 +5,14 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then 
   echo "Please provide an element as an argument."
+  exit
+fi
+
+ELEMENT=$($PSQL "SELECT name FROM elements WHERE atomic_number::text = '$1' OR symbol = '$1' OR name = '$1'")
+
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+  exit
 fi
 
